13cd6749bb8fe53740d14bac4ef7960d24ccabad
lkofe1 <denisburan5@gmail.com>
2026-06-16 23:36:03 +0400

    5

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import (
-    TagViewSet, IngredientViewSet, RecipeViewSet, CustomUserViewSet)
+from .views import TagViewSet, IngredientViewSet, RecipeViewSet
 
 router = DefaultRouter()
-router.register('users', CustomUserViewSet, basename='users')
 router.register('tags', TagViewSet, basename='tags')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register('recipes', RecipeViewSet, basename='recipes')
