596413fa3b37bad4bda94d39e788fdfd4b57d9c4
lkofe1 <denisburan5@gmail.com>
2026-06-22 03:45:57 +0400

    666

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -26,10 +26,10 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(in_favorites__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(in_shopping_cart__user=self.request.user)
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
