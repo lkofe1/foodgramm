f3c5c633a73a4b287428d1fa2c2ab6c542646caf
lkofe1 <denisburan5@gmail.com>
2026-06-22 04:09:09 +0400

    4444444

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -17,12 +17,12 @@ class RecipeFilter(filters.FilterSet):
         queryset=Tag.objects.all()
     )
     is_favorited = filters.NumberFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.NumberFilter(
-        method='filter_is_in_shopping_cart')
+    is_in_shopping_carts = filters.NumberFilter(
+        method='filter_is_shopping_carts')
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('author', 'tags', 'is_favorited', 'is_shopping_carts')
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
